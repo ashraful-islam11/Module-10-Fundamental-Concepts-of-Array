66c776d7e77e4793561eff47b46eaef9f30cce49
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-22 09:48:51 +0600

    optional topic: Array reverse method

---
+++ Module-10-7.js
@@ -0,0 +1,12 @@
+// todo:        Module no : 10-7        topic:(optional)Reversing an Array with without reverse Method.
+
+// এই টপিক টা খুব বেশি গুরুত্বপূর্ণ না। খুব বেশ use করা হবে না। 
+
+// revers method:
+
+const number = [1,3,4,45,6,66];
+console.log(number);
+
+// revers করার পরে, total Array er মধ্যে পরিবর্তন হয়ে যায়। 
+const reversNumber = number.reverse();
+console.log(number);
