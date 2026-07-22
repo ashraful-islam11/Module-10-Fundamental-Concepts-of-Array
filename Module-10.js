5c3685f28906f3d0874aaa2c053c0b0656667aca
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-22 12:06:27 +0600

    total module summery

---
+++ Module-10.js
@@ -0,0 +1,14 @@
+// todo:        Module no: 10.          topic: total module summery:
+/**
+ * todo:    যা যা শিখলাম এই মডিউল থেকে।
+ * 
+ * * 1. how to declare Array.
+ * * 2. Array declare, array index, array length.
+ * * 3. Array declare, length, get and set in Array.
+ * * 4. push, pop, shift, unshift.
+ * * 5. includes , concat, join, isArray, indexof, slice.
+ * * 6. Array Traversal Using for and while Loop( 1. for of loop. 2. normal for loop , 3. normal while loop. diye array  *      er vetore loop calano )
+ * * 7. reverse.
+ * * 8. slice, splice, sort.
+ * 
+ */
